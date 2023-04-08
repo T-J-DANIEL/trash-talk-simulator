6ad5f003c7397f9cb8900f7c25d1b8fc6c26f836
T-J-DANIEL <51197833+T-J-DANIEL@users.noreply.github.com>
2023-04-08 14:31:27 +0100

    some styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,55 +27,68 @@ const Header = () => {
   } = useGlobalContext()
   return (
     <div className="header-container">
-      {" "}
       <div className="header">
         {/* {timerExists ? <GameTimer /> : <h1>Shakespearean Wit</h1>} */}
-
-        {/* {gameState===!"paused"&&<Settings} */}
-
-        {/* <div className="header-component"></div> */}
-        <div className="header-component gameTimer">
-          {/* <TestGameTimer /> */}
-          {/* <GameTimer /> */}
-          <button
-            className="button settings-button"
-            onClick={(e) => {
-              e.getModifierState("CapsLock")
-                ? setIsCapsLockOn(true)
-                : setIsCapsLockOn(false)
-              displaySettings()
-              //pauseGame()
-            }}
-            onMouseDown={isSoundOn ? button_pop : undefined}
-            onMouseUp={isSoundOn ? button_push : undefined}
-          >
-            {/* {!showSettings ? `| | |` : "Paused"} */}| | |
-          </button>
-        </div>
-        <div className="header-component">
-          <h2 className="modal-sub-heading">Streak</h2>
-          {<div className="">{streakArray ? streakArray : "-"}</div>}
-          <h2 className="modal-sub-heading">Score</h2>
-          <div>{score}</div>
-          {/* <div className="button-container">
-            {Array.from({ length: [lives] }).map((_) => (
-              <div className="lives-left"></div>
-            ))}
-            {Array.from({ length: [3 - lives] }).map((_) => (
-              <div className="lives-left lives-lost"></div>
-            ))}
-          </div> */}
-        </div>
-        <div className="header-component">
-          <div className="button-container">
-            {Array.from({ length: [lives] }).map((_) => (
-              <div className="lives-left"></div>
-            ))}
-            {Array.from({ length: [3 - lives] }).map((_) => (
-              <div className="lives-left lives-lost"></div>
-            ))}
+        <div className="header-upper-section">
+          
+            <button
+              
+              onClick={(e) => {
+                e.getModifierState("CapsLock")
+                  ? setIsCapsLockOn(true)
+                  : setIsCapsLockOn(false)
+                displaySettings()
+                //pauseGame()
+              }}
+              onMouseDown={isSoundOn ? button_pop : undefined}
+              onMouseUp={isSoundOn ? button_push : undefined}
+            >
+             
+                <div className="settings-button">
+                  <div className="settings-button-bar"></div>
+                  <div className="settings-button-bar"></div>
+                  <div className="settings-button-bar"></div>
+                </div>
+            </button>
+          
+          <div className="header-component">
+            <div className="button-container">
+              {Array.from({ length: [lives] }).map((_) => (
+                <div className="lives-left"></div>
+              ))}
+              {Array.from({ length: [3 - lives] }).map((_) => (
+                <div className="lives-left lives-lost"></div>
+              ))}
+            </div>
+            {/* <h2 className="modal-sub-heading">Streak</h2> */}
+            {/* {<div className="">{streakArray ? streakArray : "-"}</div>} */}
+            <h2 className="modal-sub-heading">Score</h2>
+            <div>{score}</div>
+            {/* <div className="button-container">
+              {Array.from({ length: [lives] }).map((_) => (
+                <div className="lives-left"></div>
+              ))}
+              {Array.from({ length: [3 - lives] }).map((_) => (
+                <div className="lives-left lives-lost"></div>
+              ))}
+            </div> */}
           </div>
-          <TestGameTimer />
+          <div className="header-component">
+            {/* <div className="button-container">
+              {Array.from({ length: [lives] }).map((_) => (
+                <div className="lives-left"></div>
+              ))}
+              {Array.from({ length: [3 - lives] }).map((_) => (
+                <div className="lives-left lives-lost"></div>
+              ))}
+            </div> */}
+
+            <h1>Time Left</h1>
+            <TestGameTimer />
+          </div>
+        </div>
+        <div className="header-lower-section">
+          <hr /> ~^~ <hr />
         </div>
       </div>
     </div>
